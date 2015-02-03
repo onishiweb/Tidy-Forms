5a26a81a682f4c0d7e61770bd4d6fd006bf323a8
Adam Onishi <aonishi@wearearchitect.com>
2015-02-03 17:27:43 +0000

    hack(*): Add temp version of shortcode helper and hide permalink helper in post editor

--- class-tidy-forms-admin.php
+++ class-tidy-forms-admin.php
@@ -16,12 +16,21 @@ class Tidy_Forms_Admin {
 	 */
 	protected static $instance = null;
 
+	protected static $screen = null;
+
 	private function __construct() {
+
 		// Include Stylesheet/scripts
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts_styles') );
 
 		// Create post type
 		add_action( 'init', array( $this, 'setup_post_type') );
+
+		// Add meta boxes for form creation
+		add_action( 'add_meta_boxes_tidy_form', array( $this, 'setup_meta_boxes' ) );
+		add_action( 'save_post', array( $this, 'save_form_data' ) );
+
+		add_action( 'edit_form_before_permalink', array( $this, 'form_shortcode_helper' ) );
 	}
 
 	public static function get_instance() {
@@ -38,9 +47,14 @@ class Tidy_Forms_Admin {
 
 	public static function admin_scripts_styles() {
 
+		$screen = get_current_screen()->id;
+
+	    if ( 'tidy_form' === $screen ) {
+	    	wp_enqueue_style( 'tidy-forms-admin', tidy_get_dir('css/admin.css'), false, tidy_get_setting('version'), 'screen' );
+	    }
 	}
 
-	public static function setup_post_type() {
+	public function setup_post_type() {
 		 $labels = array(
 			'name'                => __('Tidy Forms', 'dirtisgood'),
 			'singular_name'       => __('Form', 'dirtisgood'),
@@ -66,12 +80,44 @@ class Tidy_Forms_Admin {
 			'hierarchical'        => false,
 			'exclude_from_search' => true,
 			'publicly_queryable'  => false,
-			'menu_position'       => 45,
+			'menu_position'       => 100,
 			'menu_icon'           => 'dashicons-feedback',
 			'supports'            => array( 'title', 'author', 'revisions' ),
 		);
 
 		register_post_type( 'tidy_form', $args );
+	}
+
+	public function setup_meta_boxes() {
+		add_meta_box(
+			'tidy-forms-settings',
+			'Form settings',
+			array( $this, 'render_settings_meta' ),
+			'tidy_form',
+			'normal',
+			'default'
+		);
+	}
+
+	public function render_settings_meta( $post ) {
+		// Include settings meta view
+	}
+
+	public function save_form_data( $post_id ) {
+
+	}
+
+	public function form_shortcode_helper( $post ) {
+
+		if( 'tidy_form' === get_current_screen()->id ) {
+			// Change this to a view
+			?>
+			<div class="inside">
+				<div id="tidy-form-shortcode-preview" class="shortcode-preview"><?php // to be filled with something useful ?></div>
+			</div>
+			<?php
+		}
+
 	}
 
 }
