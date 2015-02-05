dfe005a332e70a9342fa4f2505d3a0ce3b6c3244
Adam Onishi <aonishi@wearearchitect.com>
2015-02-05 10:21:19 +0000

    feat(*): Add ability to add a new field to the form settings

--- js/admin.js
+++ js/admin.js
@@ -1,9 +1,65 @@
-jQuery(document).on('ready', function ($) {
+// Modular JS file
+TIDY_FORMS = (function ($) {
 
-	jQuery('.tidy-fields-sortable').sortable({
-		placeholder:'ui-state-highlight'
-	});
+	// Breakpoints
+	var	init = function () {
+			// runs like jQuery normally would when everything is ready
+			// run all functions for the site inside the domReady function
+			$(domReady); // same as $(document).ready (function () {...});
+		},
 
-	jQuery('.tidy-fields-sortable').disableSelection();
+		// this runs only when we know the whole DOM is ready
+		domReady = function () {
 
-});
+			if( $('#tidy-form-fields').length ) {
+				$('#tidy-form-fields').on('click', '[tidy-action-add-field]', addField);
+			}
+
+			if( $('.tidy-fields-sortable').length ) {
+				sortableFields();
+			}
+
+			if( $('.tidy-field').length ) {
+				$('.tidy-fields').on('click', '[tidy-action-edit]', editField);
+				$('.tidy-fields').on('click', '[tidy-action-delete]', deleteField);
+			}
+
+		},
+
+		sortableFields = function() {
+			$('.tidy-fields-sortable').sortable({
+				placeholder:'tidy-sortable-placeholder'
+			});
+
+			$('.tidy-fields-sortable').disableSelection();
+		},
+
+		addField = function() {
+			var $field = $('.tidy-field-placeholder').clone(true),
+				$field_title = $field.find('th.row-title'),
+				field_count = $('.tidy-fields .tidy-field').length;
+
+
+			$field.removeClass('tidy-field-placeholder');
+			$field_title.text(field_count);
+
+			$('.tidy-fields').append($field);
+			$field.slideUp(0).slideDown('fast');
+		},
+
+		editField = function() {
+
+		},
+
+		deleteField = function() {
+
+		};
+
+	return {
+		go : init
+	};
+
+})(jQuery);
+
+TIDY_FORMS.go();
+
--- views/form-fields.php
+++ views/form-fields.php
@@ -3,40 +3,45 @@
 	<table class="widefat tidy-fields-table">
 		<thead>
 			<tr>
-				<th>#</th>
-				<th>Field name</th>
-				<th>Field description</th>
-				<th>Edit</th>
+				<th class="row-title">#</th>
+				<th class="tidy-long-col">Field name</th>
+				<th class="tidy-long-col">Field description</th>
+				<th class="tidy-action"></th>
+				<th class="tidy-action"></th>
 			</tr>
 		</thead>
 	</table>
 </div>
 
-<div class="tidy-fields-fields tidy-fields-sortable">
-	<div class="tidy-field ui-state-default">
+<div class="tidy-fields tidy-fields-sortable">
+	<div class="tidy-field">
 		<table class="widefat tidy-fields-table">
 			<tbody>
 				<tr valign="top">
 					<th class="row-title">1</th>
-					<td>Field name</td>
-					<td>Description</td>
-					<td><input type="button" class="button-secondary" value="Edit"></td>
+					<td class="tidy-long-col">Field name</td>
+					<td class="tidy-long-col">Description</td>
+					<td class="tidy-action"><input type="button" class="button-secondary" value="Edit" tidy-action-edit></td>
+					<td class="tidy-action"><input type="button" class="tidy-button-delete" value="Delete" tidy-action-delete></td>
 				</tr>
 			</tbody>
 		</table>
 	</div>
-	<div class="tidy-field ui-state-default">
+
+	<!-- Placeholder HTML to save building this all with JavaScript -->
+	<div class="tidy-field tidy-field-placeholder">
 		<table class="widefat tidy-fields-table">
 			<tbody>
 				<tr valign="top">
-					<th class="row-title">2</th>
-					<td>Field name</td>
-					<td>Description</td>
-					<td><input type="button" class="button-secondary" value="Edit"></td>
+					<th class="row-title">#</th>
+					<td class="tidy-long-col"><span>Field name</span></td>
+					<td class="tidy-long-col"><span>Description</span></td>
+					<td class="tidy-action"><input type="button" class="button-secondary" value="Edit" tidy-action-edit></td>
+					<td class="tidy-action"><input type="button" class="tidy-button-delete" value="Delete" tidy-action-delete></td>
 				</tr>
 			</tbody>
 		</table>
 	</div>
 </div>
 
-<input type="button" class="button-secondary" value="Add field">
+<input type="button" class="button-secondary" value="Add field" tidy-action-add-field>
